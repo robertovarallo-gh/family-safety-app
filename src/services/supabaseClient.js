4a76e8aa8781c9ec227a91b5de0001540563f204
robertovarallo-gh <robertovarallo@gmail.com>
2025-09-26 16:06:18 -0500

    Debug Reset v26

--- src/services/supabaseClient.js
+++ src/services/supabaseClient.js
@@ -6,4 +6,9 @@ import { createClient } from '@supabase/supabase-js';
  const supabaseUrl = 'https://brqffinwgfyixgatbmgr.supabase.co';
  const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJycWZmaW53Z2Z5aXhnYXRibWdyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTcyOTc4NDgsImV4cCI6MjA3Mjg3Mzg0OH0.WVH3XW0JVwLxUK2NFpbEqre50JHT_ebzYe1foda9s2E';
 
-export const supabase = createClient(supabaseUrl, supabaseKey);
+export const supabase = createClient(supabaseUrl, supabaseKey, {
+  auth: {
+    autoRedirectTo: false,
+    detectSessionInUrl: false
+  }
+});
