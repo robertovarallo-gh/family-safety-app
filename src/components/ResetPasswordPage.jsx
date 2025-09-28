b068f0fa604ad26327d519fd2116aab00c686b18
robertovarallo-gh <robertovarallo@gmail.com>
2025-09-28 11:26:42 -0500

    Ajuste Olvidaste Contraseña v1

--- src/components/ResetPasswordPage.jsx
+++ src/components/ResetPasswordPage.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import { useAuth } from '../context/authcontext'
+import { useAuth } from '../context/AuthContext.jsx'
 
 const ResetPasswordPage = () => {
   const { supabase } = useAuth()
