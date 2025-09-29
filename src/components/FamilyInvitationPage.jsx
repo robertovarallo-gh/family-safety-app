46a0f2e597dadcc149941f6c1da7b7aecdfda42c
robertovarallo-gh <robertovarallo@gmail.com>
2025-09-28 19:18:28 -0500

    Nueva Funccion de Crear Miembro v1

--- src/components/FamilyInvitationPage.jsx
+++ src/components/FamilyInvitationPage.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import { useAuth } from '../context/authcontext'
+import { useAuth } from '../context/AuthContext'
 import { supabase } from '../services/supabaseClient'
 
 const FamilyInvitationPage = () => {
