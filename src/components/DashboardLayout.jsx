0c2488cb21ccc030788be133307b1ddb2d3f0e62
robertovarallo-gh <robertovarallo@gmail.com>
2025-12-04 11:14:38 -0500

    Nuevo Dashboard Web v4

--- src/components/DashboardLayout.jsx
+++ src/components/DashboardLayout.jsx
@@ -58,7 +58,7 @@ const DashboardLayout = ({
         {/* Contenido principal - Izquierda */}
         <div className="flex-1 flex flex-col overflow-hidden">
           {/* Mapa - Ocupa espacio disponible */}
-          <div className="flex-1">
+          <div className="flex-1 min-h-0 relative">
             {renderMap()}
           </div>
 
