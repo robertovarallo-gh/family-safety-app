17af8d80bb77d4c739975c21a876bf8ae6be3534
robertovarallo-gh <robertovarallo@gmail.com>
2025-12-05 20:20:27 -0500

    Nuevo Dashboard Web v9

--- src/components/DashboardLayout.jsx
+++ src/components/DashboardLayout.jsx
@@ -54,11 +54,11 @@ const DashboardLayout = ({
       </div>
 
       {/* ========== DESKTOP LAYOUT ========== */}
-      <div className="hidden md:flex h-screen">
+      <div className="hidden md:flex h-screen overflow-hidden">
         {/* Contenido principal - Izquierda */}
-        <div className="flex-1 flex flex-col overflow-hidden">
+        <div className="flex-1 flex flex-col min-w-0">
           {/* Mapa - Ocupa espacio disponible */}
-          <div style={{ height: 'calc(100vh - 200px)' }} className="relative">
+          <div className="flex-1 min-h-0 relative">
             {renderMap()}
           </div>
 
